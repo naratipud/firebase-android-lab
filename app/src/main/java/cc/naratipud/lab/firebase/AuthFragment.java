691af0748ed259c04a47e154e9f881767fd29619
Naratip Udomsilp <naratip.ud@gmail.com>
2016-10-31 17:03:45 +0700

    Add Firebase authentication list

--- app/src/main/java/cc/naratipud/lab/firebase/AuthFragment.java
+++ app/src/main/java/cc/naratipud/lab/firebase/AuthFragment.java
@@ -1,29 +1,103 @@
 package cc.naratipud.lab.firebase;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+public class AuthFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class AuthFragment extends Fragment {
+    private static final Class[] CLASSES = new Class[]{
+            EmailPasswordActivity.class,
+            AnonymousAuthActivity.class
+    };
 
+    private static final int[] DESCRIPTION_IDS = new int[]{
+            R.string.desc_email_password,
+            R.string.desc_anonymous_auth,
+            R.string.desc_google_sign_in,
+            R.string.desc_facebook_login,
+            R.string.desc_twitter_login,
+            R.string.desc_firebase_ui,
+            R.string.desc_custom_auth
+    };
+
+    private ListView mListView;
 
     public AuthFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false);
+        View view = inflater.inflate(R.layout.fragment_auth, container, false);
+        mListView = (ListView) view.findViewById(R.id.list_view_chooser);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Set up ListView and Adapter
+        SimpleArrayAdapter adapter = new SimpleArrayAdapter(getContext(), android.R.layout
+                .simple_list_item_2, CLASSES);
+        adapter.setDescriptionIds(DESCRIPTION_IDS);
+
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class clicked = CLASSES[position];
+        startActivity(new Intent(getActivity(), clicked));
+    }
+
+    /**
+     * A simple Firebase authenticate array adapter
+     */
+    public static class SimpleArrayAdapter extends ArrayAdapter<Class> {
+
+        private Context mContext;
+        private Class[] mClasses;
+        private int[] mDescriptionIds;
+
+        SimpleArrayAdapter(Context context, int resource, Class[] objects) {
+            super(context, resource, objects);
+            mContext = context;
+            mClasses = objects;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            View view = convertView;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService
+                        (Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(android.R.layout.simple_list_item_2, null);
+            }
+
+            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
+            ((TextView) view.findViewById(android.R.id.text2)).setText(mDescriptionIds[position]);
+
+            return view;
+        }
+
+        private void setDescriptionIds(int[] descriptionIds) {
+            mDescriptionIds = descriptionIds;
+        }
+    }
 }
