990cff409e1e840a2524b7e3574b32ba528b0a5e
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-02 17:28:05 +0700

    First commit

---
+++ app/src/main/java/cc/naratipud/lab/firebase/models/User.java
@@ -0,0 +1,18 @@
+package cc.naratipud.lab.firebase.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    public String name;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
