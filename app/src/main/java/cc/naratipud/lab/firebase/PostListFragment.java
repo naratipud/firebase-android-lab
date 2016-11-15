413b7b306450eac1b7de5c518b3588ad82ab9e02
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-15 12:16:36 +0700

    Populate recyclerview adapter

--- app/src/main/java/cc/naratipud/lab/firebase/PostListFragment.java
+++ app/src/main/java/cc/naratipud/lab/firebase/PostListFragment.java
@@ -2,12 +2,35 @@ package cc.naratipud.lab.firebase;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class PostListFragment extends Fragment {
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.Transaction;
+
+import cc.naratipud.lab.firebase.models.Post;
+import cc.naratipud.lab.firebase.viewholder.PostViewHolder;
+
+public abstract class PostListFragment extends Fragment {
+
+    private static final String TAG = "PostListFragment";
+
+    private DatabaseReference mDatabaseRef;
+    private RecyclerView mRecyclerView;
+    private FirebaseRecyclerAdapter mAdapter;
 
     public PostListFragment() {
         // Required empty public constructor
@@ -15,8 +38,119 @@ public class PostListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_posts, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_all_posts, container, false);
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.messages_list);
+        mRecyclerView.setHasFixedSize(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Set up Layout Manager, reverse layout
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity());
+        manager.setReverseLayout(true);
+        manager.setStackFromEnd(true);
+        mRecyclerView.setLayoutManager(manager);
+
+        // Set up FirebaseRecyclerAdapter with the Query
+        Query postsQuery = getQuery(mDatabaseRef);
+        mAdapter = getAdapter(postsQuery);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mAdapter != null) {
+            mAdapter.cleanup();
+        }
+    }
+
+    public abstract Query getQuery(DatabaseReference databaseReference);
+
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    private FirebaseRecyclerAdapter getAdapter(Query postsQuery) {
+        return new FirebaseRecyclerAdapter<Post, PostViewHolder>(Post.class, R.layout
+                .item_post, PostViewHolder.class, postsQuery) {
+
+            @Override
+            protected void populateViewHolder(final PostViewHolder viewHolder, final Post model,
+                                              final int position) {
+
+                final DatabaseReference postRef = getRef(position);
+
+                // Set click listener for the whole post view
+                final String postKey = postRef.getKey();
+                viewHolder.itemView.setOnClickListener(v -> {
+                    // Launch PostDetailActivity
+//                        Intent intent = new Intent(getActivity(), PostDetailActivity.class);
+//                        intent.putExtra(PostDetailActivity.EXTRA_POST_KEY, postKey);
+//                        startActivity(intent);
+                });
+
+                // Determine if the current user has liked this post and set UI accordingly
+                if (model.stars.containsKey(getUid())) {
+                    viewHolder.starView.setImageResource(R.drawable.ic_toggle_star_24);
+                } else {
+                    viewHolder.starView.setImageResource(R.drawable.ic_toggle_star_outline_24);
+                }
+
+                // Bind Post to ViewHolder, setting OnClickListener for the star button
+                viewHolder.bindToPost(model, starView -> {
+                    // Need to write to both places the post is stored
+                    DatabaseReference globalPostRef = mDatabaseRef.child("posts").child(postRef
+                            .getKey());
+                    DatabaseReference userPostRef = mDatabaseRef.child("user-posts").child(model.uid).child(postRef.getKey());
+
+                    // Run two transactions
+                    onStarClicked(globalPostRef);
+                    onStarClicked(userPostRef);
+                });
+            }
+        };
+    }
+
+    private void onStarClicked(DatabaseReference postRef) {
+        postRef.runTransaction(new Transaction.Handler() {
+
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                Post p = mutableData.getValue(Post.class);
+                if (p == null) {
+                    return Transaction.success(mutableData);
+                }
+
+                if (p.stars.containsKey(getUid())) {
+                    // Unstar the post and remove self from stars
+                    p.starCount = p.starCount - 1;
+                    p.stars.remove(getUid());
+                } else {
+                    // Star the post and add self to stars
+                    p.starCount = p.starCount + 1;
+                    p.stars.put(getUid(), true);
+                }
+
+                // Set value and report transaction success
+                mutableData.setValue(p);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b,
+                                   DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+            }
+        });
     }
 
 }
