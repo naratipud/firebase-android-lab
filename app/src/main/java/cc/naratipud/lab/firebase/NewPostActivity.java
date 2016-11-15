fbbdbae8c78c4ded40f7204bf9dc14a19b19d143
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-15 12:19:52 +0700

    First commit

---
+++ app/src/main/java/cc/naratipud/lab/firebase/NewPostActivity.java
@@ -0,0 +1,13 @@
+package cc.naratipud.lab.firebase;
+
+import android.os.Bundle;
+
+public class NewPostActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+
+}
