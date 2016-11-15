1b998cb03fd33f11b3bf8a378f060bf4bc74ee50
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-15 18:29:24 +0700

    Update write new post

--- app/src/main/java/cc/naratipud/lab/firebase/NewPostActivity.java
+++ app/src/main/java/cc/naratipud/lab/firebase/NewPostActivity.java
@@ -1,13 +1,126 @@
 package cc.naratipud.lab.firebase;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cc.naratipud.lab.firebase.models.Post;
+import cc.naratipud.lab.firebase.models.User;
 
 public class NewPostActivity extends BaseActivity {
+
+    private static final String TAG = "NewPostActivity";
+    private static final String REQUIRED = "Required";
+
+    private DatabaseReference mDatabaseRef;
+    private RelativeLayout mRootLayout;
+    private EditText mTitleField;
+    private EditText mBodyField;
+    private FloatingActionButton mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mRootLayout = (RelativeLayout) findViewById(R.id.activity_new_post);
+        mTitleField = (EditText) findViewById(R.id.field_title);
+        mBodyField = (EditText) findViewById(R.id.field_body);
+        mSubmitButton = (FloatingActionButton) findViewById(R.id.fab_submit_post);
+
+        mSubmitButton.setOnClickListener(v -> submitPost());
+    }
+
+    private void submitPost() {
+        final String title = mTitleField.getText().toString();
+        final String body = mBodyField.getText().toString();
+
+        // Title is required
+        if (TextUtils.isEmpty(title)) {
+            mTitleField.setError(REQUIRED);
+            return;
+        }
+
+        // Body is required
+        if (TextUtils.isEmpty(body)) {
+            mBodyField.setError(REQUIRED);
+            return;
+        }
+
+        // Disable button so there are no multi-posts
+        setEditingEnabled(false);
+        Toast.makeText(this, "Posting...", Toast.LENGTH_SHORT).show();
+
+        // [START single_value_read]
+        final String userId = getUid();
+        mDatabaseRef.child("users").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get user value
+                User user = dataSnapshot.getValue(User.class);
+
+                if (user == null) {
+                    // User is null, error out
+                    Log.e(TAG, "User " + userId + " is unexpectedly null");
+                    Snackbar.make(mRootLayout, "Error: could not fetch user.", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    // Write new post
+                    writeNewPost(userId, user.username, title, body);
+                }
+
+                // Finish this Activity, back to the stream
+                setEditingEnabled(true);
+                finish();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                setEditingEnabled(true);
+            }
+
+        });
+    }
+
+    private void setEditingEnabled(boolean enabled) {
+        mTitleField.setEnabled(enabled);
+        mBodyField.setEnabled(enabled);
+        if (enabled) {
+            mSubmitButton.setVisibility(View.VISIBLE);
+        } else {
+            mSubmitButton.setVisibility(View.GONE);
+        }
+    }
+
+    private void writeNewPost(String userId, String username, String title, String body) {
+        // Create new post at /user-posts/$userid/$postid and at /posts/$postid simultaneously
+        String key = mDatabaseRef.child("posts").push().getKey();
+        Post post = new Post(userId, username, title, body);
+        Map<String, Object> postValues = post.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/posts/" + key, postValues);
+        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+        mDatabaseRef.updateChildren(childUpdates);
     }
 
 }
