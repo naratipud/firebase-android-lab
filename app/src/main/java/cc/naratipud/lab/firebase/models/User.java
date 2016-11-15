a4df687e2cfa496b6209e80140df662f148a3e83
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-15 15:59:15 +0700

    Change name to username

--- app/src/main/java/cc/naratipud/lab/firebase/models/User.java
+++ app/src/main/java/cc/naratipud/lab/firebase/models/User.java
@@ -4,15 +4,16 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class User {
-    public String name;
+
+    public String username;
     public String email;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String name, String email) {
-        this.name = name;
+    public User(String username, String email) {
+        this.username = username;
         this.email = email;
     }
 }
