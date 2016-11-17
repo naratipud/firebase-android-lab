bdef1e59571eab9ed76d199b63238b0b11fcb63d
Naratip Udomsilp <naratip.ud@gmail.com>
2016-11-17 10:58:27 +0700

    Add post detail & comment list

--- app/src/main/java/cc/naratipud/lab/firebase/PostDetailActivity.java
+++ app/src/main/java/cc/naratipud/lab/firebase/PostDetailActivity.java
@@ -1,16 +1,300 @@
 package cc.naratipud.lab.firebase;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PostDetailActivity extends BaseActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cc.naratipud.lab.firebase.models.Comment;
+import cc.naratipud.lab.firebase.models.Post;
+import cc.naratipud.lab.firebase.models.User;
+
+public class PostDetailActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = PostDetailActivity.class.getSimpleName();
     public static final String EXTRA_POST_KEY = "post_key";
+
+    private DatabaseReference mPostReference;
+    private DatabaseReference mCommentsReference;
+    private ValueEventListener mPostListener;
+    private CommentAdapter mAdapter;
+
+    private RelativeLayout mRootLayout;
+    private TextView mAuthorView;
+    private TextView mTitleView;
+    private TextView mBodyView;
+    private EditText mCommentField;
+    private RecyclerView mCommentsRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
+
+        // Get post key from intent
+        String postKey = getIntent().getStringExtra(EXTRA_POST_KEY);
+        if (postKey == null) {
+            throw new IllegalArgumentException("Must pass EXTRA_POST_KEY");
+        }
+
+        // Initialize Database
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("posts").child(postKey);
+        mCommentsReference = FirebaseDatabase.getInstance().getReference().child("post-comments").child(postKey);
+
+        bindViews();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Add value event listener to the post
+        mPostListener = new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+
+                mAuthorView.setText(post.author);
+                mTitleView.setText(post.title);
+                mBodyView.setText(post.body);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                Snackbar.make(mRootLayout, "Failed to load post.", Snackbar.LENGTH_SHORT).show();
+            }
+        };
+
+        mPostReference.addValueEventListener(mPostListener);
+
+        // Listen for comments
+        mAdapter = new CommentAdapter(this, mCommentsReference);
+        mCommentsRecycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Remove post value event listener
+        if (mPostListener != null) {
+            mPostReference.removeEventListener(mPostListener);
+        }
+
+        // Clean up comments listener
+        mAdapter.cleanupListener();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_post_comment:
+                postComment();
+                break;
+        }
+    }
+
+    private void bindViews() {
+        mRootLayout = (RelativeLayout) findViewById(R.id.activity_post_detail);
+        mAuthorView = (TextView) findViewById(R.id.post_author);
+        mTitleView = (TextView) findViewById(R.id.post_title);
+        mBodyView = (TextView) findViewById(R.id.post_body);
+        mCommentField = (EditText) findViewById(R.id.field_comment_text);
+        mCommentsRecycler = (RecyclerView) findViewById(R.id.recycler_comments);
+
+        findViewById(R.id.button_post_comment).setOnClickListener(this);
+        mCommentsRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void postComment() {
+        final String uid = getUid();
+        FirebaseDatabase.getInstance().getReference().child("users").child(uid)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user information
+                        User user = dataSnapshot.getValue(User.class);
+                        String authorName = user.username;
+
+                        // Create new comment object
+                        String commentText = mCommentField.getText().toString();
+                        Comment comment = new Comment(uid, authorName, commentText);
+
+                        // Push the comment, it will appear in the list
+                        mCommentsReference.push().setValue(comment);
+
+                        // Clear the field
+                        mCommentField.setText(null);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.d(TAG, "postComment:onCancelled:" + databaseError);
+                    }
+
+                });
+    }
+
+    /**
+     * Comment view holder.
+     */
+    private static class CommentViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView authorView;
+        private TextView bodyView;
+
+        CommentViewHolder(View itemView) {
+            super(itemView);
+
+            authorView = (TextView) itemView.findViewById(R.id.comment_author);
+            bodyView = (TextView) itemView.findViewById(R.id.comment_body);
+        }
+    }
+
+    /**
+     * Comment adapter.
+     */
+    private static class CommentAdapter extends RecyclerView.Adapter<CommentViewHolder> {
+
+        private Context mContext;
+        private DatabaseReference mDatabaseReference;
+        private ChildEventListener mChildEventListener;
+
+        private List<String> mCommentIds = new ArrayList<>();
+        private List<Comment> mComments = new ArrayList<>();
+
+        CommentAdapter(Context context, DatabaseReference databaseReference) {
+            mContext = context;
+            mDatabaseReference = databaseReference;
+            mChildEventListener = getChildEventListener();
+            mDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+
+        @Override
+        public CommentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.item_comment, parent, false);
+            return new CommentViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CommentViewHolder holder, int position) {
+            Comment comment = mComments.get(position);
+            holder.authorView.setText(comment.author);
+            holder.bodyView.setText(comment.text);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mComments.size();
+        }
+
+        void cleanupListener() {
+            if (mChildEventListener != null) {
+                mDatabaseReference.removeEventListener(mChildEventListener);
+            }
+        }
+
+        private ChildEventListener getChildEventListener() {
+            return new ChildEventListener() {
+
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                    // A new comment has been added, add it to the displayed list
+                    Comment comment = dataSnapshot.getValue(Comment.class);
+
+                    // Update RecyclerView
+                    mCommentIds.add(dataSnapshot.getKey());
+                    mComments.add(comment);
+                    notifyItemInserted(mComments.size() - 1);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                    // A comment has changed, use the key to determine if we are displaying this
+                    // comment and if so displayed the changed comment.
+                    Comment newComment = dataSnapshot.getValue(Comment.class);
+                    String commentKey = dataSnapshot.getKey();
+
+                    int commentIndex = mCommentIds.indexOf(commentKey);
+                    if (commentIndex > -1) {
+                        // Replace with the new data
+                        mComments.set(commentIndex, newComment);
+
+                        // Update the RecyclerView
+                        notifyItemChanged(commentIndex);
+                    } else {
+                        Log.w(TAG, "onChildChanged:unknown_child:" + commentKey);
+                    }
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                    // A comment has changed, use the key to determine if we are displaying this
+                    // comment and if so remove it.
+                    String commentKey = dataSnapshot.getKey();
+
+                    int commentIndex = mCommentIds.indexOf(commentKey);
+                    if (commentIndex > -1) {
+                        // Remove data from the list
+                        mCommentIds.remove(commentIndex);
+                        mComments.remove(commentIndex);
+
+                        // Update the RecyclerView
+                        notifyItemRemoved(commentIndex);
+                    } else {
+                        Log.w(TAG, "onChildRemoved:unknown_child:" + commentKey);
+                    }
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                    // A comment has changed position, use the key to determine if we are
+                    // displaying this comment and if so move it.
+//                    Comment movedComment = dataSnapshot.getValue(Comment.class);
+//                    String commentKey = dataSnapshot.getKey();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                    Toast.makeText(mContext, "Failed to load comments.", Toast.LENGTH_SHORT).show();
+                }
+            };
+        }
+
     }
 
 }
